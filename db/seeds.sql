c72c9f89aef05938c12e49635cdcd591007be980
karleighponnock <karleighponnock@gmail.com>
2020-07-18 13:16:00 -0400

    css body

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Hawaiian Burger');
+INSERT INTO burgers (name, eaten) VALUES ('BBQ Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Chili Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Sliders', true);
+INSERT INTO burgers (name) VALUES ('Mushroom and Onion Burger');
