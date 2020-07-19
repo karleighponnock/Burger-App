3e968ae42489056bfc511f90ee56980939418c15
karleighponnock <karleighponnock@gmail.com>
2020-07-18 21:31:34 -0400

    css and delete button working

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
-INSERT INTO burgers (name) VALUES ('Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Hawaiian Burger');
-INSERT INTO burgers (name, eaten) VALUES ('BBQ Burger', true);
-INSERT INTO burgers (name, eaten) VALUES ('Chili Burger', true);
-INSERT INTO burgers (name, eaten) VALUES ('Sliders', true);
-INSERT INTO burgers (name) VALUES ('Mushroom and Onion Burger');
+
+INSERT INTO burgers (name) VALUES ("Cheeseburger");
+INSERT INTO burgers (name) VALUES ("Hawaiian Burger");
+INSERT INTO burgers (name, eaten) VALUES ("BBQ Burger", true);
+INSERT INTO burgers (name, eaten) VALUES ("Chili Burger", true);
+INSERT INTO burgers (name, eaten) VALUES ("Sliders", true);
+INSERT INTO burgers (name) VALUES ("Mushroom Burger");
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,34 +1,26 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".change-eaten").on("click", function(event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
+    var newEat = $(this).data("neweaten");
 
-    var newSleepState = {
-      eaten: newSleep
+    var newEatState = {
+      eaten: newEat
     };
 
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: newEatState
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        console.log("changed eaten to", newEat);
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
 
-  $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-
-    var newBurger = {
-      name: $("#ca").val().trim(),
-      eaten: $("[name=eaten]:checked").val().trim()
-    };
 
     // Send the POST request.
     $.ajax("/api/burgers", {
@@ -41,7 +33,7 @@ $(function() {
         location.reload();
       }
     );
-  });
+ 
 
   $(".delete-burger").on("click", function(event) {
     var id = $(this).data("id");
